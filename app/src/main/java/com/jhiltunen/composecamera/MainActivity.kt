560d214e9ebc56b29474caa910025e03d802f203
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-01-31 20:31:01 +0200

    Added functionality to take thumbnail picture

--- app/src/main/java/com/jhiltunen/composecamera/MainActivity.kt
+++ app/src/main/java/com/jhiltunen/composecamera/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.jhiltunen.composecamera
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.Image
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jhiltunen.composecamera.ui.theme.ComposeCameraTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    GetThumbnail()
                 }
             }
         }
@@ -30,14 +42,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun GetThumbnail() {
+    val result = remember { mutableStateOf<Bitmap?>(null) }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) {
+        result.value = it
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeCameraTheme {
-        Greeting("Android")
+    Column(modifier = Modifier
+        .padding(10.dp)
+        .fillMaxWidth()) {
+        IconButton(onClick = { launcher.launch() }, modifier = Modifier.fillMaxWidth()) {
+            Row() {
+                Icon(
+                    Icons.Filled.Add,
+                    contentDescription = "Menu"
+                )
+                Text(text = "Take a picture")
+            }
+        }
+        Spacer(Modifier.height(16.dp))
+        result.value?.let { image ->
+            Image(bitmap = image.asImageBitmap(), contentDescription = null, modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight())
+        }
     }
 }
